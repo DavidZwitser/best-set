7687291c02c50073fedc93f4186b57fac77bfa0c
DavidZwitser <davidzwitser@gmail.com>
2018-03-23 12:52:58 +0100

    Made the save data class static

--- ts/BackEnd/SaveData.ts
+++ ts/BackEnd/SaveData.ts
@@ -10,50 +10,50 @@ export default class SaveData
     private static StorageKey: string = 'bs-saveData';
 
     /* Set if the sfx are muted in cache */
-    set SFXMuted(value: boolean)
+    public static set SFXMuted(value: boolean)
     {
         let newData: ISaveData = this.data;
         newData.sm = value;
 
         this.data = newData;
     }
-    get SFXMuted(): boolean
+    public static get SFXMuted(): boolean
     {
         return this.data.sm;
     }
 
     /* Save if the sound is muted in cache */
-    set musicMuted(value: boolean)
+    public static set musicMuted(value: boolean)
     {
         let newData: ISaveData = this.data;
         newData.mm = value;
 
         this.data = newData;
     }
-    get musicMuted(): boolean
+    public static get musicMuted(): boolean
     {
         return this.data.mm;
     }
 
     /* Set highscore in cache */
-    set highscore(value: number)
+    public static set highscore(value: number)
     {
         let newData: ISaveData = this.data;
         newData.hs = value;
 
         this.data = newData;
     }
-    get highscore(): number
+    public static get highscore(): number
     {
         return this.data.hs;
     }
 
     /* Set or get the cached data */
-    set data(data: ISaveData)
+    private static set data(data: ISaveData)
     {
         localStorage.setItem(SaveData.StorageKey, JSON.stringify(data));
     }
-    get data(): ISaveData
+    private static get data(): ISaveData
     {
         return JSON.parse(localStorage.getItem(SaveData.StorageKey));
     }
